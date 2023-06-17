4f85511d0c74c66d1107573b1e49022a1e0b540c
yysuen <yysuen.me@gmail.com>
2023-06-18 02:02:42 +0800

    fix: comment path error

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -6,22 +6,18 @@ import '@waline/client/dist/waline.css'
 
 const data = useData()
 const serverURL = data.site.value.themeConfig.walineServerURL
-const path = computed(() => useRoute().path)
 const lang = navigator.language ?? 'zh-CN'
+const path = computed(() => {
+  // posts/example/index => posts/example
+  // posts/example/index.html => posts/example
+  return useRoute().path.replace(/(\/)?(index)?(\.html)?$/, '')
+})
 const shouldShowWaline = computed(() => {
   return serverURL && data.page.value.frontmatter.comments !== false && !data.page.value.isNotFound
 })
 </script>
 
 <template>
-  <!-- <div v-show="shouldShowWaline" id="waline">
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/@waline/client@v2/dist/waline.css"
-      >
-    </Head>
-  </div> -->
   <Waline v-if="shouldShowWaline" :server-u-r-l="serverURL" :path="path" :lang="lang" dark="body.dark" />
 </template>
 
